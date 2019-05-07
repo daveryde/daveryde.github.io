bae0baf5654ec9531581dcba4411e38449d3f0ed
daveryde <d.mcordova87@gmail.com>
2019-05-07 10:51:51 -0700

    create responsive navigation

---
+++ js/menu.js
@@ -0,0 +1,31 @@
+function start() {
+    const menuBtn = document.querySelector(".menu-btn");
+    const menu = document.querySelector('.menu');
+    const menuNav = document.querySelector('.menu-nav');
+    const navItems = document.querySelectorAll('.nav-item');
+    
+    let showMenu = false;
+    
+    menuBtn.addEventListener('click', toggleMenu);
+    
+    function toggleMenu() {
+        if(!showMenu) {
+            menuBtn.classList.add('close');
+            menu.classList.add('show');
+            menuNav.classList.add('show');
+            navItems.forEach(item => item.classList.add('show'));
+    
+            showMenu = true;
+        } else {
+            menuBtn.classList.remove('close');
+            menu.classList.remove('show');
+            menuNav.classList.remove('show');
+            navItems.forEach(item => item.classList.remove('show'));
+    
+            showMenu = false;
+        }
+    }
+}
+
+document.addEventListener('load', start(), false);
+
